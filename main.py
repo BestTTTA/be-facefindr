a00801fe5f9d96e04e5f9d759d3a8d54bfa27046
BestTTTA <bst12546@gmail.com>
2025-06-29 14:16:47 +0700

    Test on Face TTTA

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-SERVER_BASE_URL = "https://be-facefindr.thetigerteamacademy.net"
+SERVER_BASE_URL = "https://be-facettta.thetigerteamacademy.net"
 
 # Initialize FaceFindr
 face_finder = FaceFindr(db_path="facedb.sqlite")
